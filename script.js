274a881a5f73a9f6cb0ba16918e6b96e4dc9dd10
Guilherme <test@test.com.au.br>
2020-10-04 13:15:10 +1100

    Instructors Page Caroucel

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 function onLoad() {
-    const btn = document.querySelector('.navbutton')
-    const nav = document.querySelector('.navMenu')
-    const a = document.querySelector('.navBtn')
-    //const nav = document.querySelector('.navigation')
-    // make btn respond to a click
-    btn.addEventListener('click', function() {
-      if( nav.classList.contains('open') && a.classList.contains('open') ){
-        nav.classList.remove('open')
-        a.classList.remove('open')
-      }
-      else {
-        nav.classList.add('open')
-        a.classList.add('open')
-      }
-    })
-  }
+  const btn = document.querySelector('.navbutton')
+  const nav = document.querySelector('.navMenu')
+  const a = document.querySelector('.navBtn')
+  //const nav = document.querySelector('.navigation')
+  btn.addEventListener('click', function() {
+    if( nav.classList.contains('open') && a.classList.contains('open') ){
+      nav.classList.remove('open')
+      a.classList.remove('open')
+    }
+    else {
+      nav.classList.add('open')
+      a.classList.add('open')
+    }
+  })
+}
   
-  window.addEventListener("load", onLoad )
+window.addEventListener("load", onLoad )
+
+var slideIndex = 0;
+showSlides();
+  
+function showSlides() {
+  var counter;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("index-dot");
+  for (counter = 0; counter < slides.length; counter++) {
+      slides[counter].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex> slides.length) {slideIndex = 1}    
+  for (counter = 0; counter < dots.length; counter++) {
+      dots[counter].className = dots[counter].className.replace(" active-dot", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active-dot";
+  setTimeout(showSlides, 5000);
+}
